a2fdf3cf775675473965c3e8c24ff5fc831317f5
DB <37939411+Unisio@users.noreply.github.com>
2018-10-24 16:58:04 +0200

    RSS Feed
    Added RSS FEed

--- curly-Winner/projektet/projektet/Form1.cs
+++ curly-Winner/projektet/projektet/Form1.cs
@@ -13,13 +13,30 @@ namespace projektet
             InitializeComponent();
         }
 
+        String[,] rssData = null;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string[] lista = new string[] { };
 
 
-            string path = @"D:\text.txt";
+            listBox1.Items.Clear();
+            rssData = getRssData(tbUrl.Text);
+            for (int i = 0; i < rssData.GetLength(0); i++) {
+
+                if (rssData[i, 0] != null) {
+                    listBox1.Items.Add(rssData[i, 0]);
+
+
+                }
+
+                listBox1.SelectedIndex = 0;
+            }
+
+
+            //string[] lista = new string[] { };
+
+
+            // string path = @"D:\text.txt";
             //if (File.Exists(path))
             //{
             //    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
@@ -29,33 +46,33 @@ namespace projektet
             //            foreach (var länk in lista)
             //            {
             //                sre.ReadLine() += länk;
-                         
-                        
+
+
             //            }
             //        }
             //    }
             //    Close();
             //}
-            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                using (StreamWriter sr = new StreamWriter(fs))
-                {
+            //    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            //    {
+            //        using (StreamWriter sr = new StreamWriter(fs))
+            //        {
 
-                    var nyLänk = tbUrl.Text;
-                    Array.Resize(ref lista, lista.Length + 1);
-                
-                    for (int i = 0; i < lista.Length; i++)
-                    {
-                        lista[lista.Length-1] = nyLänk;
-                    }
+            //            var nyLänk = tbUrl.Text;
+            //            Array.Resize(ref lista, lista.Length + 1);
 
-
+            //            for (int i = 0; i < lista.Length; i++)
+            //            {
+            //                lista[lista.Length-1] = nyLänk;
+            //            }
 
 
-                    Close();
 
-                }
-            }
+
+            //            Close();
+
+            //        }
+            //    }
         }
 
 
@@ -81,7 +98,73 @@ namespace projektet
             }
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
 
+
+
+            if (rssData[listBox1.SelectedIndex, 1] != null)
+
+                DescriptionTextBox.Text = rssData[listBox1.SelectedIndex, 1];
+            if (rssData[listBox1.SelectedIndex, 2] != null)
+                linkLabel.Text = "Follow the link: " + rssData[listBox1.SelectedIndex, 0];
+
+
+        }
+
+        private String[,] getRssData(String channel) {
+            System.Net.WebRequest myRequest = System.Net.WebRequest.Create(channel);
+            System.Net.WebResponse myResponse = myRequest.GetResponse();
+
+            System.IO.Stream rssStream = myResponse.GetResponseStream();
+            System.Xml.XmlDocument rssDoc = new System.Xml.XmlDocument();
+
+            rssDoc.Load(rssStream);
+            System.Xml.XmlNodeList rssItems = rssDoc.SelectNodes("rss/channel/item");
+
+            String[,] tempRssData = new String[100, 3];
+
+            for (int i = 0; i < rssItems.Count; i++) {
+
+                System.Xml.XmlNode rssNode;
+                rssNode = rssItems.Item(i).SelectSingleNode("title");
+
+                if (rssNode != null) {
+                    tempRssData[i, 0] = rssNode.InnerText;
+
+                } else {
+                    tempRssData[i, 0] = "";
+
+                }
+
+                rssNode = rssItems.Item(i).SelectSingleNode("description");
+                if (rssNode != null) {
+                    tempRssData[i, 1] = rssNode.InnerText;
+                } else {
+                    tempRssData[i, 1] = "";
+                }
+
+                rssNode = rssItems.Item(i).SelectSingleNode("link");
+                if (rssNode != null) {
+                    tempRssData[i, 2] = rssNode.InnerText;
+
+                } else {
+                    tempRssData[i, 2] = "";
+                }
+
+
+            }
+            return tempRssData;
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void tbUrl_TextChanged(object sender, EventArgs e) {
+
+        }
     }
-}
+    }
+
 
